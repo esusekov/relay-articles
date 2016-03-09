1d93990fc12877e9a81fb593f56fb2630dc8a9d9
Egor Susekov <esusekov@gmail.com>
2016-03-09 10:37:29 +0300

    remove comment

--- js/redux/reducers/index.js
+++ js/redux/reducers/index.js
@@ -2,13 +2,6 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import articles from './articles';
 
-/**
- * Combination of several reducers
- * http://redux.js.org/docs/basics/Reducers.html
- * http://redux.js.org/docs/api/combineReducers.html
- *
- * @type {Function}
- */
 const rootReducer = combineReducers({
   articles,
   routing: routerReducer
